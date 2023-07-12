a54e02a7572a5e6d0d5f2fbaa2553f13bd4206fa
unknown <danilka2712@yandex.ru>
2023-07-12 14:32:29 +0600

    first commit

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -7,7 +7,7 @@
         class="mx-auto flex max-w-7xl items-center justify-between p-4 py-8 lg:py-4 lg:px-0"
         aria-label="Global"
     >
-        <div class="flex lg:flex-1">
+        <div class="flex lg:flex-1 z-40">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">Выкуп авто</span>
                 <img class="h-10 w-auto" src="/logotype.svg" alt="" />
@@ -50,11 +50,7 @@
             <div
                 class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#f8f8f8] px-4 py-8 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
             >
-                <div class="flex items-center justify-between">
-                    <a href="#" class="-m-1.5 p-1.5">
-                        <span class="sr-only">Выкуп авто</span>
-                        <img class="h-10 w-auto" src="/logotype.svg" alt="" />
-                    </a>
+                <div class="flex items-center justify-end">
                     <button
                         on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
                         type="button"
