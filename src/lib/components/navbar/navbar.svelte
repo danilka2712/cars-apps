fd38400c961c96f6c35828de04f2a704d45a7a18
unknown <danilka2712@yandex.ru>
2023-07-12 14:30:33 +0600

    first commit

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -48,7 +48,7 @@
             <!-- Background backdrop, show/hide based on slide-over state. -->
             <div class="fixed inset-0 z-10" />
             <div
-                class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#f8f8f8] py-8 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+                class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#f8f8f8] px-4 py-8 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
             >
                 <div class="flex items-center justify-between">
                     <a href="#" class="-m-1.5 p-1.5">
